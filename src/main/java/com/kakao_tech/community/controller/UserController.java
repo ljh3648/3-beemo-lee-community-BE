d4418a4b7d3f23e3e879f542393e0a5195ee5efe
Lee Jihun <ljh09060@gmail.com>
2025-10-10 15:05:29 +0900

    docs: TODO 주석 추가

--- src/main/java/com/kakao_tech/community/controller/UserController.java
+++ src/main/java/com/kakao_tech/community/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.kakao_tech.community.controller;
 
 import com.kakao_tech.community.dto.UserDTO;
-import com.kakao_tech.community.repository.UserRepository;
 import com.kakao_tech.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +19,10 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<UserDTO.SignUpResponse> createUser(@RequestBody UserDTO.SignUpRequset userDTO) {
-        UserDTO.SignUpResponse result  = userService.createUser(userDTO);
+    public ResponseEntity<UserDTO.SignUpResponse> createUser(@RequestBody UserDTO.SignUpRequest userDTO) {
+        // TODO : 추후에 전역 핸들러를 통해서 예외처리 필요, 서비스에서 여기는 정상 응답만 반환하기.
+        // TODO : 예외처리에 대한 고민이 필요함.
+        UserDTO.SignUpResponse result = userService.createUser(userDTO);
         return ResponseEntity.status(201).body(result);
     }
-
 }
