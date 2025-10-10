db60f05708c322eec34d4700d11172c288c49a76
Lee Jihun <ljh09060@gmail.com>
2025-10-10 21:32:18 +0900

    feat: 세션레포지토리 추가

---
+++ src/main/java/com/kakao_tech/community/repository/SessionRepository.java
@@ -0,0 +1,9 @@
+package com.kakao_tech.community.repository;
+
+import com.kakao_tech.community.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+}
