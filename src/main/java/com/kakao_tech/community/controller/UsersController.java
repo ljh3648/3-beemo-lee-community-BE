614ed3285608bda10096ede13f14590f837b1907
Lee Jihun <ljh09060@gmail.com>
2025-10-11 19:45:28 +0900

    refactor: 일부 코드 정리

--- src/main/java/com/kakao_tech/community/controller/UsersController.java
+++ src/main/java/com/kakao_tech/community/controller/UsersController.java
@@ -25,26 +25,22 @@ public class UsersController {
     @PostMapping("/users")
     public ResponseEntity<?> createUser(@RequestBody Map<String, String> body) {
         // 원래 DTO로 받았는데 그러면 try catch 문에서 예외처리가 안걸림.
-        String nickname = body.get("nickname");
-        String email = body.get("email");
-        String password = body.get("password");
-        String profileUrl = body.get("profileUrl");
-
         // TODO : 추후에 전역 핸들러를 통해서 예외처리 필요, 서비스에서 여기는 정상 응답만 반환하기.
         // TODO : 예외처리에 대한 고민이 필요함.
         try {
-            UserDTO.SignUpRequest userDTO = new UserDTO.SignUpRequest(nickname, email, password, profileUrl);
+            UserDTO.SignUpRequest userDTO =
+                    new UserDTO.SignUpRequest(
+                            body.get("nickname"),
+                            body.get("email"),
+                            body.get("password"),
+                            body.get("profileUrl")
+                            );
 
             UserDTO.SignUpResponse result = userService.createUser(userDTO);
             return ResponseEntity.status(201).body(result);
         } catch (IllegalArgumentException e) {
-            // 원래 여기에 바디에 이런식으로 하려고 했는데
-            // "error": "ERROR_EMAIL_NULL",
-            // "message": "이메일이 입력되지 않았습니다."
-            // 일단 해보자
             ErrorDTO errorDTO;
 
-            // 인자오류 응답 반환
             if (e.getMessage().equals("닉네임 NULL 오류")) {
                 errorDTO = new ErrorDTO("ERROR_NICKNAME_IS_NULL", e.getMessage());
                 return ResponseEntity.status(400).body(errorDTO);
@@ -65,7 +61,6 @@ public class UsersController {
                 return ResponseEntity.status(400).body(errorDTO);
             }
 
-            // 중복 응답 반환
             if (e.getMessage().equals("중복된 유저 닉네임 입니다.")) {
                 errorDTO = new ErrorDTO("ERROR_NICKNAME_DUPLICATION", e.getMessage());
                 return ResponseEntity.status(400).body(errorDTO);
