a85cb983c00d83c82d30d3823c9a0ac6ae9f7150
Lee Jihun <ljh09060@gmail.com>
2025-10-19 17:58:15 +0900

    feat: header css 로드 추가

--- web-server/public/components/header/header.js
+++ web-server/public/components/header/header.js
@@ -1,4 +1,11 @@
 const fetchHeader = async () => {
+    // CSS 로드
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/components/header/header.css';
+    document.head.appendChild(link);
+
+    // HTML 로드
     const res = await fetch('/components/header/header.html');
     const header = await res.text();
     return header;
