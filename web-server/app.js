b8a719f3606a53313ac489f1411a954943a5c598
Leejihun <ljh09060@gmail.com>
2025-10-22 19:28:57 +0900

    원시시대로 롤백

--- web-server/app.js
+++ web-server/app.js
@@ -8,33 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/signin/index.html'));
-});
-
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/signup/index.html'));
-});
-
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/home/index.html'));
-});
-
-app.get('/posts/create', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/posts/create/index.html'));
-});
-
-app.get('/posts/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/posts/detail/index.html'));
-});
-
-app.get('/posts/:id/edit', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pages/posts/edit/index.html'));
-});
-
-app.use(express.static('public/'))
-app.use(express.static('public/pages/'))
-
 app.use((req, res) => {
   res.status(404).send('페이지를 찾을 수 없습니다.');
 });
