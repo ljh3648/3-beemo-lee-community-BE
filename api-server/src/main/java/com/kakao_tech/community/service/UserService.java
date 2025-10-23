40b9278f85b2792e4120546f08a4c328a0e94e20
Leejihun <ljh09060@gmail.com>
2025-10-23 19:56:15 +0900

    [BE] feat: 이미지 로컬 저장기능 추가 (데이터베이스 URL 저장은 아직 미구현)

--- api-server/src/main/java/com/kakao_tech/community/service/UserService.java
+++ api-server/src/main/java/com/kakao_tech/community/service/UserService.java
@@ -1,25 +1,25 @@
 package com.kakao_tech.community.service;
 
-import com.kakao_tech.community.dto.ImageDTO;
 import com.kakao_tech.community.dto.UserDTO;
 import com.kakao_tech.community.entity.User;
 import com.kakao_tech.community.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 //@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final ImageService imageService;
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ImageService imageService) {
         this.userRepository = userRepository;
+        this.imageService = imageService;
     }
 
-    // TODO : 프로필 사진 어떻게 저장할건지?
-    public UserDTO.SignUpResponse createUser(UserDTO.SignUpRequest userDTO) {
+    public UserDTO.SignUpResponse createUser(UserDTO.SignUpRequest userDTO, MultipartFile profileImage) {
         if (userRepository.existsByNickname(userDTO.getNickname())) {
             throw new IllegalArgumentException("중복된 유저 닉네임 입니다.");
         }
@@ -34,7 +34,20 @@ public class UserService {
                 BCrypt.hashpw(userDTO.getPassword(), BCrypt.gensalt()),
                 userDTO.getProfileUrl()
                 );
+
         user = userRepository.save(user);
+
+        if (profileImage != null) {
+            user.setProfileUrl(
+                    imageService.uploadImage(
+                            profileImage,
+                            "users/" + user.getId() + "/profile/",
+                            "profile_"+ profileImage.getOriginalFilename()
+                    )
+            );
+            userRepository.save(user);
+        }
+
 
         return new UserDTO.SignUpResponse(user);
     }
