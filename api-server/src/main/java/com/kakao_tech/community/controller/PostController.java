46566602da3e2c5a6d890f51683fa1cd45822506
Lee Jihun <ljh09060@gmail.com>
2025-10-27 01:18:04 +0900

    [BE] feat: posts get 라우팅 추가

--- api-server/src/main/java/com/kakao_tech/community/controller/PostController.java
+++ api-server/src/main/java/com/kakao_tech/community/controller/PostController.java
@@ -1,4 +1,29 @@
 package com.kakao_tech.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kakao_tech.community.dto.PostDTO;
+import com.kakao_tech.community.service.PostService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    @Autowired
+    public final PostService postService;
+
+    @GetMapping
+    // /posts?limit=10&lastPostId=10
+    public ResponseEntity<PostDTO.ListResponse> getPosts(@RequestParam(defaultValue = "10") int limit, @RequestParam(required = false) Long lastPostId){
+        // 게시물 리스트 받기
+        return ResponseEntity.status(200).build();
+    }
 }
