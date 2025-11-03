c2f94d7e709b3ac1b261da551363e5cfb448a9e9
Leejihun <ljh09060@gmail.com>
2025-11-03 20:25:22 +0900

    feat: 응답 DTO 추가

--- src/main/java/com/kakao_tech/community/dto/ImageDTO.java
+++ src/main/java/com/kakao_tech/community/dto/ImageDTO.java
@@ -1,27 +1,21 @@
 package com.kakao_tech.community.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class ImageDTO {
 
     @Getter
-    public static class UploadRequest{
+    @AllArgsConstructor
+    public static class UploadRequest {
         private final String path;
         private final String fileName;
         private final String data;
-
-
-        public UploadRequest(String path, String fileName, String data) {
-            this.path = path;
-            this.fileName = fileName;
-            this.data = data;
-        }
     }
 
-    public static class UploadResponse{
+    @Getter
+    @AllArgsConstructor
+    public static class UploadResponse {
+        private final String imageUrl;
     }
-
-    public static class GetResponse{
-    }
-
 }
