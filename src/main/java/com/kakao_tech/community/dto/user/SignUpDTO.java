d94233d0c3220da1aa3ad4e1cbe690fa71673ae7
Leejihun <ljh09060@gmail.com>
2025-11-03 19:34:21 +0900

    feat: 검증 어노테이션 적용

--- src/main/java/com/kakao_tech/community/dto/user/SignUpDTO.java
+++ src/main/java/com/kakao_tech/community/dto/user/SignUpDTO.java
@@ -1,7 +1,9 @@
 package com.kakao_tech.community.dto.user;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 public class SignUpDTO {
@@ -9,8 +11,16 @@ public class SignUpDTO {
     @Getter
     @AllArgsConstructor
     public static class Request {
+        @NotBlank(message = "{required.user.nickname}")
         private String nickname;
+
+        @NotBlank(message = "{requir`required.user.password}")
+        @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$", message = "{invalid.user.email}")
+        @Size(max = 320, message = "{invalid.user.email}")
         private String email;
+
+        @NotBlank(message = "{required.user.password}")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$", message = "{invalid.user.password}")
         private String password;
         private String profileUrl;
 
@@ -18,13 +28,11 @@ public class SignUpDTO {
     }
 
     @Getter
-    @Builder
     @AllArgsConstructor
-    public static class SignUpResponse {
-        private Integer id;
+    public static class Response {
+        private Integer userId;
         private String nickname;
         private String email;
         private String profileUrl;
     }
-
 }
